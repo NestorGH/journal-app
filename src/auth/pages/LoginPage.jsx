ec1aee43d87589091ff4bded41e7011bc47ba3da
NestorCastro <nestornica10@gmail.com>
2022-10-09 21:16:16 -0600

    First commit

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,74 @@
+import { Link, Link as RouterLink } from 'react-router-dom'
+import { Google } from "@mui/icons-material"
+import { Button, Grid, TextField, Typography } from "@mui/material"
+
+export const LoginPage = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}>
+
+      <Grid item
+        className="box-shadow"
+        xs={3}
+        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
+
+        <Typography variant="h5" sx={{ mb: 1 }}>Login</Typography>
+
+        <form>
+
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label='Correo'
+                type='email'
+                placeholder='correo@algo.com'
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label='Contraseña'
+                type='password'
+                placeholder='password'
+                fullWidth
+              />
+            </Grid>
+
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+
+              <Grid item xs={12} sm={6}>
+                <Button variant='contained' fullWidth>
+                  Login
+                </Button>
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <Button variant='contained' fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+
+            </Grid>
+
+            <Grid container direction='row' justifyContent='end'>
+              <Link component={RouterLink} color='inherit' to='/auth/register'>
+                Create account
+              </Link>
+            </Grid>
+
+          </Grid>
+
+        </form>
+
+      </Grid>
+
+    </Grid>
+  )
+}
