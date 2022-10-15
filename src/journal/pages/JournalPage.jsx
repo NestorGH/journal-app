f1f64731a7f385487e4076eca43fe5f004c2a428
NestorCastro <nestornica10@gmail.com>
2022-10-14 21:56:46 -0600

    Journal page

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 
@@ -6,8 +7,24 @@ import { NoteView, NothingSelectedView } from '../views'
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView/> */}
+      {/* <NothingSelectedView /> */}
       <NoteView/>
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main', ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+
+        <AddOutlined sx={{ fontSize: 30 }} />
+
+      </IconButton>
+
     </JournalLayout>
   )
 }
